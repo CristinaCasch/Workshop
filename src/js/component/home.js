3fa53536e37ad6aef1b40f8ada38461de42aa263
CristinaCasch <criscastro1926@hotmail.com>
2021-03-20 22:33:59 +0000

    Todo-list-Fetch

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,124 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export const Home = () => {
+	let [lista, setLista] = useState([]);
 
-//create your first component
-export function Home() {
+	useEffect(() => {
+		fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/CristinaCasch",
+			{
+				method: "POST",
+				body: JSON.stringify([]),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		).then(resp => {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/CristinaCasch",
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json"
+					}
+				}
+			)
+				.then(resp => {
+					console.log("respuesta", resp);
+					return resp.json();
+				})
+				.then(data => {
+					setLista(data);
+				})
+				.catch(error => {
+					//manejo de errores
+					console.log(error);
+				});
+		});
+	}, []);
+
+	const addTarea = tarea => {
+		if (tarea.key === "Enter") {
+			let newObj = {
+				label: tarea.target.value,
+				done: false
+			};
+			setLista([...lista, newObj]);
+			tarea.target.value = "";
+		}
+	};
+
+	function delTarea(pos) {
+		const tempList = [...lista];
+		tempList.splice(pos, 1);
+		const methods = ["PUT", "DELETE"];
+		if (tempList.length > 0) {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/CristinaCasch",
+				{
+					method: methods[0],
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(tempList)
+				}
+			)
+				.then(resp => {
+					setLista(tempList);
+				})
+				.catch(error => {
+					console.log("Error delete", error);
+				});
+		} else {
+			fetch(
+				"https://assets.breatheco.de/apis/fake/todos/user/CristinaCasch",
+				{
+					method: methods[1],
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(tempList)
+				}
+			)
+				.then(resp => {
+					console.log("Respuesta de borrado", resp);
+					setLista(tempList);
+					console.log(lista);
+				})
+				.catch(error => {
+					console.log("Error delete", error);
+				});
+		}
+	}
+
+	const newList = lista.map((value, index) => (
+		<li className="list-group-item" key={index}>
+			{value.label}
+			<button
+				onClick={() => delTarea(index)}
+				className="btn btn-default remove-item float-end">
+				<i className="fas fa-trash-alt text-danger mx-2"></i>
+			</button>
+		</li>
+	));
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container">
+				<div className="row">
+					<div className="col-12 col-md-6">
+						<h1 className="text-muted">To Do List</h1>
+						<input
+							onKeyDown={addTarea}
+							className="form-control"
+							type="text"
+							placeholder="Add a thing"></input>
+						<br />
+						<ul className="list-group">{newList}</ul>
+					</div>
+				</div>
+			</div>
+			<p>Total Items: {lista.length}</p>
 		</div>
 	);
-}
+};
